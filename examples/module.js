9f37be4afd59c7844aa6ccf0631afb9179d0eb50
Alex J Burke <alex@alexjeffburke.com>
2020-12-23 15:25:20 +0100

    Add a nested import within the examples.

--- examples/module.js
+++ examples/module.js
@@ -1,3 +1,14 @@
-import { html, render } from 'https://unpkg.com/htm/preact/standalone.module.js'
+import {
+    html,
+    render
+} from "https://unpkg.com/htm/preact/standalone.module.js";
 
-render(html`<h1>Hello from "${document.location.pathname}"</h1>`, document.getElementById('app-root'))
+import { content } from "./nested.js";
+
+render(
+    html`
+        <h1>Hello from "${document.location.pathname}"</h1>
+        <span>${content}</span>
+    `,
+    document.getElementById("app-root")
+);
