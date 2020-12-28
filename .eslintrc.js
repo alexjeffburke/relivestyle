23077f6e657fe4676786966f83c3413965a713ed
Alex J Burke <alex@alexjeffburke.com>
2020-12-28 15:22:19 +0100

    Tighten eslint setup and bump versions.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,31 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2019
     },
-    env: {
-        mocha: true
+    plugins: ["import", "mocha"],
+    extends: ["standard", "prettier", "prettier/standard"],
+    rules: {
+        "import/no-unresolved": ["error", { commonjs: true }],
+        "prefer-template": "error",
+        "prefer-const": "error"
     },
-    extends: ["standard", "prettier", "prettier/standard"]
+    overrides: [
+        {
+            files: ["examples/*.js"],
+            rules: {
+                "import/no-unresolved": "off"
+            }
+        },
+        {
+            env: {
+                mocha: true
+            },
+            files: ["*.spec.js"],
+            rules: {
+                "no-new": "off",
+                "mocha/no-exclusive-tests": "error",
+                "mocha/no-nested-tests": "error",
+                "mocha/no-identical-title": "error"
+            }
+        }
+    ]
 };
