512a0a9539dba7c4f12d68196ad91e4033cb444b
Alex J Burke <alex@alexjeffburke.com>
2021-01-01 23:01:43 +0100

    Split out the node imports rewrite logic.

---
+++ test/rewriteNodeImports.spec.js
@@ -0,0 +1,68 @@
+const expect = require("unexpected")
+    .clone()
+    .use(require("unexpected-snapshot"));
+const path = require("path");
+
+const rewriteNodeImports = require("../lib/rewriteNodeImports");
+
+const ROOT_DIR = path.join(__dirname, "..");
+
+describe("rewriteNodeImports", () => {
+    it("should rewrite node_modules imports with single quote", async () => {
+        const input = "import bits from 'htm/preact';";
+
+        const output = await rewriteNodeImports(input, ROOT_DIR);
+
+        expect(
+            output,
+            "to equal snapshot",
+            expect.unindent`
+            import bits from '/__node_modules/htm/preact/index.module.js';
+            `
+        );
+    });
+
+    it("should rewrite node_modules imports with double quote", async () => {
+        const input = 'import bits from "htm/preact";';
+
+        const output = await rewriteNodeImports(input, ROOT_DIR);
+
+        expect(
+            output,
+            "to equal snapshot",
+            expect.unindent`
+            import bits from "/__node_modules/htm/preact/index.module.js";
+            `
+        );
+    });
+
+    it("should rewrite multiple node_modules imports", async () => {
+        const input =
+            'import bits from "htm/preact";\nimport bits from "htm/preact";';
+
+        const output = await rewriteNodeImports(input, ROOT_DIR);
+
+        expect(
+            output,
+            "to equal snapshot",
+            expect.unindent`
+            import bits from "/__node_modules/htm/preact/index.module.js";
+            import bits from "/__node_modules/htm/preact/index.module.js";
+            `
+        );
+    });
+
+    it("should rewrite namespaced node_modules imports", async () => {
+        const input = 'import bits from "@depository/store";';
+
+        const output = await rewriteNodeImports(input, ROOT_DIR);
+
+        expect(
+            output,
+            "to equal snapshot",
+            expect.unindent`
+            import bits from "/__node_modules/@depository/store/src/index.js";
+            `
+        );
+    });
+});
