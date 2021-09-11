f228518fac5bdd182486bf11585dbaed256afd8e
Alex J Burke <alex@alexjeffburke.com>
2021-09-11 08:25:38 +0200

    Consistently create middleware per-test in the assetMiddleware spec.

--- test/assetMiddleware.spec.js
+++ test/assetMiddleware.spec.js
@@ -2,7 +2,6 @@ const expect = require("unexpected")
   .clone()
   .use(require("unexpected-express"))
   .use(require("unexpected-snapshot"));
-const fs = require("fs");
 const path = require("path");
 const sinon = require("sinon");
 
@@ -12,9 +11,12 @@ const PathMonitor = require("../lib/PathMonitor");
 const { determineNearestNodeModules } = require("../lib/tasteServePath");
 
 const TEST_DATA = path.join(__dirname, "..", "testdata");
+const TEST_DATA_EXAMPLE_DELICATE = path.join(TEST_DATA, "example-delicate");
+const TEST_DATA_EXAMPLE_INDEX = path.join(TEST_DATA, "example-index");
 const TEST_DATA_EXAMPLE_IMAGE = path.join(TEST_DATA, "example-image");
 const TEST_DATA_EXAMPLE_MODULE = path.join(TEST_DATA, "example-module");
 const TEST_DATA_EXAMPLE_NPM = path.join(TEST_DATA, "example-npm");
+const TEST_DATA_EXAMPLE_PROJECT = path.join(TEST_DATA, "example-project");
 const TEST_DATA_EXAMPLE_RELATIONS = path.join(TEST_DATA, "example-relations");
 const TEST_DATA_EXAMPLE_LERNA_DEMO_DIR = path.join(
   TEST_DATA,
@@ -35,25 +37,27 @@ function toNodeModulesPath(servePath) {
 
 describe("asset middleware", function() {
   describe("when serving node_modules", function() {
-    let middleware;
-
-    function middlewareForServePath(servePath) {
+    function middlewareForServePath(servePath, options) {
+      options = options || {};
       const nodeModulesPath = toNodeModulesPath(servePath);
-      const importResolver = new ImportResolver({ servePath, nodeModulesPath });
-      const result = createMiddleware({
+
+      const importResolver = new ImportResolver({
+        ...options.importResolver,
+        servePath,
+        nodeModulesPath
+      });
+
+      return createMiddleware({
         importResolver,
         pathMonitor: {},
         servePath,
         nodeModulesPath
       });
-      return result.middleware;
     }
 
-    beforeEach(() => {
-      middleware = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
-    });
-
     it("should respond with a 200 and content-type of application/javascript", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/unexpected/build/lib/index.js"
@@ -68,6 +72,8 @@ describe("asset middleware", function() {
     });
 
     it("should respond for a nested path within a namespaced package", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/@depository/store/dist/store.esm.js"
@@ -81,89 +87,9 @@ describe("asset middleware", function() {
       });
     });
 
-    it("should respond for a hoisted path within a workspace", async function() {
-      const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
-      const nodeModulesPath = toNodeModulesPath(servePath);
-      const { middleware } = createMiddleware({
-        importResolver: new ImportResolver({
-          isMonorepo: true,
-          servePath,
-          nodeModulesPath
-        }),
-        pathMonitor: {},
-        servePath,
-        nodeModulesPath
-      });
-
-      await expect(middleware, "to yield exchange", {
-        request: {
-          url: "/__node_modules/~/3/node_modules/htm/preact/index.module.js"
-        },
-        response: {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "application/javascript; charset=utf-8"
-          }
-        }
-      });
-    });
-
-    it("should respond for a hoisted namespaced path within a workspace", async function() {
-      const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
-      const nodeModulesPath = toNodeModulesPath(servePath);
-      const { middleware } = createMiddleware({
-        importResolver: new ImportResolver({
-          isMonorepo: true,
-          servePath,
-          nodeModulesPath
-        }),
-        pathMonitor: {},
-        servePath,
-        nodeModulesPath
-      });
-
-      await expect(middleware, "to yield exchange", {
-        request: {
-          url:
-            "/__node_modules/~/3/node_modules/@nano-router/router/src/index.js"
-        },
-        response: {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "application/javascript; charset=utf-8"
-          }
-        }
-      });
-    });
-
-    it("should respond for a nested path within a workspace", async function() {
-      const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
-      const nodeModulesPath = toNodeModulesPath(servePath);
-      const { middleware } = createMiddleware({
-        importResolver: new ImportResolver({
-          isMonorepo: true,
-          servePath,
-          nodeModulesPath
-        }),
-        pathMonitor: {},
-        servePath,
-        nodeModulesPath
-      });
-
-      await expect(middleware, "to yield exchange", {
-        request: {
-          url: "/__node_modules/~/1/packages/internals/index.js"
-        },
-        response: {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "application/javascript; charset=utf-8"
-          }
-        }
-      });
-    });
-
     it("should respond with rewritten paths", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/htm/preact/index.module.js"
@@ -175,6 +101,8 @@ describe("asset middleware", function() {
     });
 
     it("should respond for the internal socket module", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/sockette/dist/sockette.min.js"
@@ -189,6 +117,8 @@ describe("asset middleware", function() {
     });
 
     it("should respond for the local client code", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/relivestyle/lib/frontend/client.js"
@@ -203,6 +133,8 @@ describe("asset middleware", function() {
     });
 
     it("should respond with a 404 for a missing file", async function() {
+      const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_NPM);
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/__node_modules/@depository/store/dist/index.js"
@@ -214,11 +146,9 @@ describe("asset middleware", function() {
     });
 
     describe("when node_modules has non-JavaScript content", () => {
-      beforeEach(() => {
-        middleware = middlewareForServePath(TEST_DATA_EXAMPLE_IMAGE);
-      });
-
       it("should serve an image/gif", async function() {
+        const { middleware } = middlewareForServePath(TEST_DATA_EXAMPLE_IMAGE);
+
         await expect(middleware, "to yield exchange", {
           request: {
             url: "/__node_modules/catimages/lasercat.gif"
@@ -239,32 +169,95 @@ describe("asset middleware", function() {
         });
       });
     });
+
+    describe("when within a workspace", () => {
+      it("should respond for a hoisted path", async function() {
+        const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
+        const { middleware } = middlewareForServePath(servePath, {
+          importResolver: {
+            isMonorepo: true
+          }
+        });
+
+        await expect(middleware, "to yield exchange", {
+          request: {
+            url: "/__node_modules/~/3/node_modules/htm/preact/index.module.js"
+          },
+          response: {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/javascript; charset=utf-8"
+            }
+          }
+        });
+      });
+
+      it("should respond for a hoisted namespaced path", async function() {
+        const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
+        const { middleware } = middlewareForServePath(servePath, {
+          importResolver: {
+            isMonorepo: true
+          }
+        });
+
+        await expect(middleware, "to yield exchange", {
+          request: {
+            url:
+              "/__node_modules/~/3/node_modules/@nano-router/router/src/index.js"
+          },
+          response: {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/javascript; charset=utf-8"
+            }
+          }
+        });
+      });
+
+      it("should respond for a nested path", async function() {
+        const servePath = TEST_DATA_EXAMPLE_LERNA_DEMO_DIR;
+        const { middleware } = middlewareForServePath(servePath, {
+          importResolver: {
+            isMonorepo: true
+          }
+        });
+
+        await expect(middleware, "to yield exchange", {
+          request: {
+            url: "/__node_modules/~/1/packages/internals/index.js"
+          },
+          response: {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/javascript; charset=utf-8"
+            }
+          }
+        });
+      });
+    });
   });
 
   describe("when serving HTML", function() {
-    const htmlPath = path.join(TEST_DATA_EXAMPLE_MODULE, "stuff.html");
-    const htmlContent = fs.readFileSync(htmlPath, "utf8");
     let pathMonitor;
-    let middleware;
 
     beforeEach(function() {
       pathMonitor = createMockPathMonitor();
       pathMonitor.loadAsset.resolves({
         asset: {
-          text: htmlContent
+          text: "<html></html>"
         },
         get hash() {
           return 123;
         }
       });
-      const result = createMiddleware({
-        servePath: TEST_DATA_EXAMPLE_MODULE,
-        pathMonitor
-      });
-      middleware = result.middleware;
     });
 
     it("should respond with a 200 and content-type of text/html", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/stuff.html"
@@ -279,6 +272,11 @@ describe("asset middleware", function() {
     });
 
     it("should respond with the content adding the socket client", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/stuff.html"
@@ -291,7 +289,7 @@ describe("asset middleware", function() {
 
     it("should respond with index.html on a request for /", async function() {
       const { middleware } = createMiddleware({
-        servePath: path.join(TEST_DATA, "example-index"),
+        servePath: TEST_DATA_EXAMPLE_INDEX,
         pathMonitor
       });
 
@@ -307,7 +305,7 @@ describe("asset middleware", function() {
 
     it("should respond with the html on a request without extension", async function() {
       const { middleware } = createMiddleware({
-        servePath: path.join(TEST_DATA, "example-project"),
+        servePath: TEST_DATA_EXAMPLE_PROJECT,
         pathMonitor
       });
 
@@ -322,6 +320,11 @@ describe("asset middleware", function() {
     });
 
     it("should respond with a 404 if the file does not exist", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/"
@@ -333,6 +336,10 @@ describe("asset middleware", function() {
     });
 
     it("should respond with a 404 if loading fails", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
       pathMonitor.loadAsset.resolves(null);
 
       await expect(middleware, "to yield exchange", {
@@ -346,6 +353,10 @@ describe("asset middleware", function() {
     });
 
     it("should respond with a 304 on an ETag match", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
       pathMonitor.getAsset.returns({ hash: 123 });
 
       await expect(middleware, "to yield exchange", {
@@ -362,6 +373,10 @@ describe("asset middleware", function() {
     });
 
     it("should not respond with a 304 on a ETag mismatch", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
       pathMonitor.getAsset.returns({ hash: 456 });
 
       await expect(middleware, "to yield exchange", {
@@ -380,7 +395,7 @@ describe("asset middleware", function() {
     describe("when permitting client side routing", () => {
       it("should respond with index.html if the file does not exist", async () => {
         const { middleware } = createMiddleware({
-          servePath: path.join(TEST_DATA, "example-index"),
+          servePath: TEST_DATA_EXAMPLE_INDEX,
           pathMonitor,
           permitClientSideRouting: true
         });
@@ -398,26 +413,23 @@ describe("asset middleware", function() {
   });
 
   describe("when serving JS", function() {
-    const jsPath = path.join(TEST_DATA_EXAMPLE_NPM, "stuff.js");
-    const jsContent = fs.readFileSync(jsPath, "utf8");
     let pathMonitor;
-    let middleware;
 
     beforeEach(function() {
       pathMonitor = createMockPathMonitor();
       pathMonitor.loadAsset.resolves({
         asset: {
-          text: jsContent
+          text: "(function () {})()"
         }
       });
-      const result = createMiddleware({
-        servePath: TEST_DATA_EXAMPLE_MODULE,
-        pathMonitor
-      });
-      middleware = result.middleware;
     });
 
     it("should respond with a 200 and content-type of application/json", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
+
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/stuff.js"
@@ -432,6 +444,10 @@ describe("asset middleware", function() {
     });
 
     it("should respond with a 404 if loading fails", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_MODULE,
+        pathMonitor
+      });
       pathMonitor.loadAsset.resolves(null);
 
       await expect(middleware, "to yield exchange", {
@@ -446,26 +462,23 @@ describe("asset middleware", function() {
   });
 
   describe("when serving CSS", function() {
-    const filePath = path.join(TEST_DATA_EXAMPLE_RELATIONS, "stuff.css");
-    const fileContent = fs.readFileSync(filePath, "utf8");
     let pathMonitor;
-    let middleware;
 
     beforeEach(function() {
       pathMonitor = createMockPathMonitor();
       pathMonitor.loadAsset.resolves({
         asset: {
-          text: fileContent
+          text: "body {}"
         }
       });
-      const result = createMiddleware({
+    });
+
+    it("should respond with a 200 and content-type of text/css", async function() {
+      const { middleware } = createMiddleware({
         servePath: TEST_DATA_EXAMPLE_RELATIONS,
         pathMonitor
       });
-      middleware = result.middleware;
-    });
 
-    it("should respond with a 200 and content-type of application/json", async function() {
       await expect(middleware, "to yield exchange", {
         request: {
           url: "/stuff.css"
@@ -478,12 +491,29 @@ describe("asset middleware", function() {
         }
       });
     });
+
+    it("should respond with a 404 if loading fails", async function() {
+      const { middleware } = createMiddleware({
+        servePath: TEST_DATA_EXAMPLE_RELATIONS,
+        pathMonitor
+      });
+      pathMonitor.loadAsset.resolves(null);
+
+      await expect(middleware, "to yield exchange", {
+        request: {
+          url: "/xxx.css"
+        },
+        response: {
+          statusCode: 404
+        }
+      });
+    });
   });
 
   describe("when paired with a real PathMonitor", () => {
     describe("when serving HTML", () => {
       it("should correctly generate an ETag", async function() {
-        const servePath = path.join(TEST_DATA, "example-index");
+        const servePath = TEST_DATA_EXAMPLE_INDEX;
         const { middleware } = createMiddleware({
           servePath,
           pathMonitor: new PathMonitor({
@@ -511,7 +541,7 @@ describe("asset middleware", function() {
 
     describe("when serving JS", () => {
       it("should correctly generate an ETag", async function() {
-        const servePath = path.join(TEST_DATA, "example-delicate");
+        const servePath = TEST_DATA_EXAMPLE_DELICATE;
         const { middleware } = createMiddleware({
           servePath,
           pathMonitor: new PathMonitor({
@@ -539,7 +569,7 @@ describe("asset middleware", function() {
 
     describe("when serving CSS", () => {
       it("should correctly generate an ETag", async function() {
-        const servePath = path.join(TEST_DATA, "example-relations");
+        const servePath = TEST_DATA_EXAMPLE_RELATIONS;
         const { middleware } = createMiddleware({
           servePath,
           pathMonitor: new PathMonitor({
